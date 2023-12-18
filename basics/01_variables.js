280a95886e0b148554ae98728dd590f26da0818c
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-18 11:26:03 +0000

    Variables done

---
+++ basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=144423
+
+ let accountEmail="abc@gmail.com"
+
+ var accountPass="abvsdf"
+
+ accountCity="Pratapgarh"
+
+//  accountId=745875
+
+console.table([accountId,accountEmail,accountPass,accountCity])
