d94015b2f036a8ffbaa0389116013da6378e0fa0
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-18 15:36:51 +0000

    Datatypes

---
+++ basics/03_datatypes.js
@@ -0,0 +1,12 @@
+//  let v="abcd"    string datatype
+ 
+// let v=123      Numbers datatype
+
+// let b=true     boolean datatype 
+
+// let b;         undefined
+
+// let b=null      null 
+console.log(typeof(null));
+
+console.log(typeof(undefined));
