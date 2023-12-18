bbb98693ae10f1fb0ea2f704b01af525bee6dc4c
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-18 15:56:16 +0000

    Datatype conversion done

---
+++ basics/03_datatypeconversion.js
@@ -0,0 +1,35 @@
+// let b="234"
+
+// let v= Number(b)
+
+// console.log(typeof v)
+
+// let b="234bb"
+
+// let v= Number(b)
+
+// console.log(v)
+
+// let b=null
+
+// let v= Number(b)
+
+// console.log( v)
+
+// let b=undefined
+
+// let v= Number(b)
+
+// console.log( v)
+
+// let b=""
+
+// let v= Boolean(b)
+
+// console.log( v)
+
+let b="Naveen"
+
+let v= Boolean(b)
+
+console.log( v)
