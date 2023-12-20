2699b75af2874f1f400330e9350b4588968202a8
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-20 19:50:17 +0000

    String done

---
+++ basics/06_strings.js
@@ -0,0 +1,36 @@
+// const name='Naveen'
+
+// const age=22
+
+// console.log(`hii my name is ${name} and my age is ${age}`)
+
+// const name=new String('Naveen')
+
+// console.log(name.length)
+
+// console.log(name.charAt(2))
+
+// console.log(name.indexOf('e'))
+
+// console.log(name.substring(0,3))
+
+// console.log(name.slice(-8,4))
+
+
+// const name="   Naveen  "
+
+// console.log(name)
+
+// console.log(name.trim())
+
+const name="Naveen-shera-nn"
+
+console.log(name.includes('t'))
+
+console.log(name.split('-'))
+
+
+
+
+
+
