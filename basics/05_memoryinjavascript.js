05678679a726feeb467ce234d7d12ac0b20a767f
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-20 11:42:00 +0000

    memory done

---
+++ basics/05_memoryinjavascript.js
@@ -0,0 +1,27 @@
+//  Stack Memory Concept in Js {All Primitive Datatypes}
+
+ let u1="ram"
+
+ let u2=u1
+
+ u2="sita"
+
+ console.log(u1)
+
+ console.log(u2)
+
+//  Heap Memory Concept in Js {All Non-Primitive Datatypes}
+
+ let v={
+
+    pass:"abcd"
+ }
+ 
+
+ let n=v
+
+ n.pass="1234"
+
+ console.log(v.pass)
+
+ console.log(n.pass)
