47fa35654a5b3e337802ceb488f98b1617306f6a
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-21 20:36:09 +0000

    array done

---
+++ basics2/01_array.js
@@ -0,0 +1,31 @@
+// const a=[0,1,2,3,4]
+
+// console.log(a)
+
+const a=new Array(1,2,3,4)
+
+// console.log(a)
+  
+//  a.push(6)
+
+// console.log(a)
+
+// a.pop()
+
+// console.log(a)
+
+// a.shift(0)
+
+// console.log(a)
+
+ console.log(a.includes(2))
+
+ console.log("A",a)
+
+ console.log(a.slice(1,3))
+
+ console.log("B",a)
+
+ console.log(a.splice(1,3))
+
+ console.log("C",a)
