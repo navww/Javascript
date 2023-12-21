dee12479bda1bffd293cb7cc229a4d47903f67ec
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-21 16:22:01 +0000

    Number and Math done

---
+++ basics/07_nums_and_maths.js
@@ -0,0 +1,23 @@
+// const s=400
+
+// console.log(s)
+
+// const b=new Number(400)
+
+// console.log(b)
+
+//  console.log(Math.abs(-4))
+
+//  console.log(Math.round(4.3))
+
+//  console.log(Math.ceil(4.2))
+
+//  console.log(Math.floor(4.2))
+
+//  Random Function 
+
+const min=10
+
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
