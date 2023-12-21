00faa774f75bc59404f407b51c7d8643b3e00562
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-21 20:44:44 +0000

    aaray done

---
+++ basics2/02_array.js
@@ -0,0 +1,33 @@
+const a1=[1,2,3]
+
+const a2=[4,5,6]
+
+// a1.push(a2)
+
+// console.log(a1)
+
+// const a3=a1.concat(a2)
+
+// console.log(a3)
+
+// const z=[...a1,...a2]  // we using  spread operator 
+
+// console.log(z)
+
+// const a=[1,2,[3,4],5,[6,[7,8]]]
+
+// const n=a.flat(Infinity)
+
+// console.log(n)
+
+console.log(Array.isArray("Naveen"))
+
+console.log(Array.from("Naveen"))
+
+const a=100
+
+const b=200
+
+const c=300
+
+console.log(Array.of(a,b,c))
