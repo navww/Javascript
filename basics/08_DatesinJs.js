a7a90b6d4d4a6710fd7dc23876da1a165fd3a9e3
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-21 19:14:25 +0000

    Date and Time Done

---
+++ basics/08_DatesinJs.js
@@ -0,0 +1,22 @@
+// const d= new Date()
+
+// console.log(d)
+
+// console.log(d.toString())
+
+// console.log(d.toDateString())
+
+// console.log(d.toLocaleString())
+
+// console.log(typeof d)
+
+// const d=new Date(2023,11,22,5,3)
+
+// console.log(d.toDateString())
+
+// const d=new Date("2023-12-22")
+
+// console.log(d.toLocaleString())
+
+console.log(Math.floor(Date.now()/1000))
+
