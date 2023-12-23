ff64cd5c58788dce1e0a51f8f8b0f20c3b8e3446
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-23 18:19:32 +0000

    function done

---
+++ basics2/05_function.js
@@ -0,0 +1,42 @@
+// function name(){
+
+//      console.log("What is your name?")
+
+// }
+
+//        name()
+
+// function sum(a,b){
+
+//     return a+b
+
+// }
+
+//        console.log(sum(3,4));
+
+//    function num(...n){
+
+//      return n
+//    }
+
+//     console.log(num(200,300,400))
+
+//   function objs(anyo){
+
+//       console.log(`hii mr ${anyo.username } and My age ${anyo.age}`);
+
+//   }
+
+//     objs({
+//        username:"Naveen",
+
+//        age:18
+
+//     })
+
+function arr(geta){
+
+     return geta[0]
+}
+
+   console.log(arr([100 ,200,300]));
