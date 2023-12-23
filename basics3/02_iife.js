db28f88982b19fd7bad3174b95eee94c957acb0b
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-23 20:52:25 +0000

    iife done

---
+++ basics3/02_iife.js
@@ -0,0 +1,19 @@
+// function chai(){
+ 
+//      console.log("chai and code")
+
+// }
+
+//  chai()
+
+(function chai(){
+ 
+     console.log("chai and code")
+
+})();
+
+ ((name)=>{
+
+     console.log(`${name},Welcome to website `)
+ })("Naveen")
+ 
