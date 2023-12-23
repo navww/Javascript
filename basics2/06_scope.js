ac4d1fc87b8701974656f11236249a3193699418
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-23 19:23:40 +0000

    Scope Done

---
+++ basics2/06_scope.js
@@ -0,0 +1,54 @@
+// var c=300
+
+// if(true){
+//  let a=100
+
+// const b=200
+
+// var c=30
+// }
+
+// console.log(a)
+
+// console.log(b)
+
+// console.log(c)
+
+// function one(){
+
+//      let user="Naveen"
+
+//      function two(){
+   
+//           let web="Youtube"
+          
+//            console.log(user)
+//      }
+
+//       two()
+
+// }
+
+//   one()
+
+// console.log(one(5))
+
+function one(v){
+
+    return v+1
+
+
+}
+
+// console.log(one(5))
+
+console.log(user(5))
+
+let user=function two(v){
+
+    return v+2
+
+
+}
+
+// console.log(user(5))
