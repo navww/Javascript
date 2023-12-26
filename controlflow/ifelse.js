353cb5f1e7553174f4b1fbf7b7dcc8cd7105a14d
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-26 20:35:19 +0000

    controlflow done

---
+++ controlflow/ifelse.js
@@ -0,0 +1,52 @@
+// const a=[]
+
+// if(a.length==0){
+
+//     console.log(" Array is empty ")
+// }
+
+//  else{
+
+//     console.log(" Array is not empty ");
+//  }
+
+// const a={}
+
+// if(Object.keys(a).length==0){
+
+//     console.log(" Object is empty ")
+// }
+
+//  else{
+
+//     console.log(" Object is not empty ");
+//  }
+
+
+//  let v1;
+
+//  v1=5??10;
+
+//  console.log(v1)
+
+//  let v1;
+
+//  v1=null??10;
+
+//  console.log(v1)
+
+//  let v2;
+
+//  v2=undefined??5;
+
+//  console.log(v2)
+
+// let v2;
+
+//  v2=undefined??5??10;
+
+//  console.log(v2)
+
+const i=10
+
+i>=5?console.log("i is greater than 5"):console.log(" i is not greater than 5 ")
