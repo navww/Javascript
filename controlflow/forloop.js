7800623664b0b979a2f493e6e64d9211c29796aa
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-26 20:42:27 +0000

    for loop done

---
+++ controlflow/forloop.js
@@ -0,0 +1,51 @@
+// for(let i=1;i<=10;i++){
+
+//      const e=i;
+
+//      if(e==5){
+
+//         console.log(" 5 is the best number ")
+
+//      }
+
+//      console.log(e)
+
+// }
+
+// const m=['A','B','C']
+
+// for(let i=0;i<m.length;i++){
+
+//      console.log(`Alphabets are ${m[i]}`)
+
+// }
+
+// for(let i=1;i<=10;i++){
+
+//     const e=i;
+
+//     if(e==5){
+
+//        console.log(" 5 is the best number ")
+
+//          break;
+//     }
+
+//     console.log(e)
+
+// }
+
+for(let i=1;i<=10;i++){
+
+    const e=i;
+
+    if(e==5){
+
+       console.log(" 5 is the best number ")
+
+        continue;
+    }
+
+    console.log(e)
+
+}
