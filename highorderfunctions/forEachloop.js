67b74373495a45e000a1c58d47f40cc4da991488
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-29 20:42:29 +0000

    for Each loop done

---
+++ highorderfunctions/forEachloop.js
@@ -0,0 +1,26 @@
+const a=[1,2,3,4]
+
+// a.forEach(function(i){
+
+//     console.log(i)
+// })
+
+a.forEach((i)=>{
+
+    // console.log(i)
+
+})
+
+function print(i){
+
+    console.log(i)
+}
+
+// a.forEach(print)
+
+const b=[{name:"Naveen",age:25},{name:"Ram",age:28},{name:"Pallavi",age:22}]
+
+    b.forEach((i)=>{
+
+        console.log(i.name)
+    })
