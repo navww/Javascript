2fd8b033d73c3887e2fb756b095dfa500f1cd442
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-29 20:34:37 +0000

    for in loop done

---
+++ controlflow/forinloop.js
@@ -0,0 +1,25 @@
+const o={
+
+    name:"Naveen",
+
+    age:18
+}
+
+for(i in o){
+
+    //console.log(i)
+}
+
+for(i in o){
+
+    // console.log(o[i])
+}
+
+const a=[1,2,3,4]
+
+for(const i in a){
+
+    // console.log(i)
+    console.log(a[i])
+}
+
