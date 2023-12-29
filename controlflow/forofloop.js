6f5038edd211d9152599422cccc5152d867974c4
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-29 20:34:57 +0000

    for of loop done

---
+++ controlflow/forofloop.js
@@ -0,0 +1,22 @@
+const a=[1,2,3,4]
+
+for(const i of a){
+
+    // console.log(i)
+}
+
+
+const m=new Map()
+
+m.set('IN',"India")
+
+m.set('US',"America")
+
+m.set('FR',"France")
+
+for(const [i,n] of m){
+
+      
+    console.log(i,"->",n)
+    
+}
