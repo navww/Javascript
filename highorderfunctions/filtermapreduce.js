854cf240fc73be1d4817e58528025188100ec65d
Naveen Tripathi <95501363+Navww@users.noreply.github.com>
2023-12-30 21:28:52 +0000

    High order functions done

---
+++ highorderfunctions/filtermapreduce.js
@@ -0,0 +1,23 @@
+// const a=[1,2,3,4,5]
+
+// const b=a.filter((i)=>i>2)
+
+//  console.log(b)
+
+// const a=[1,2,3,4]
+
+// const b=a.map((i)=>i+4)
+
+// console.log(b)
+
+// const a=[1,2,3,4]
+
+// const r=a.reduce((ac,i)=>ac+i,0)
+
+// console.log(r)
+
+const a=[1,2,3,4]
+
+const b=a.map((i)=>i*2).filter((i)=>i>2)
+
+console.log(b)
